5097922bce10f5ece97e967f8842203fb062305a
Bren Rodriguez <brendaerodriguez@hotmail.com>
2023-10-24 23:51:35 -0300

    agrego iconos de redes sociales y ajustes en el navbar y home

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="card" style="width: 300px;">
-  <!-- <img src="..." class="card-img-top" alt="..."> -->
-  <div class="card-body">
-    <h3 class="card-title">{{ evento.titulo }}</h3>
-    <h5 class="card-text">{{ evento.descripcion }}</h5>
-    <p class="card-text">Precio:${{ evento.precio }}</p>
-    <p class="card-text">Fecha:{{ evento.fecha }}</p>
-    <p class="card-text">Inscriptos:{{ evento.cantidad }}</p>
-    <a href="#" class="btn btn-primary">Anotarme</a>
+    <!-- <img src="..." class="card-img-top" alt="..."> -->
+    <div class="card-body">
+      <h3 class="card-title">{{ evento.titulo }}</h3>
+      <h5 class="card-text">{{ evento.descripcion }}</h5>
+      <p class="card-text">Precio:${{ evento.precio }}</p>
+      <p class="card-text">Fecha:{{ evento.fecha }}</p>
+      <p class="card-text">Inscriptos:{{ evento.cantidad }}</p>
+      <a href="#" class="btn btn-primary">Anotarme</a>
+    </div>
   </div>
-</div>
-
 </template>
 
 <script setup>
 const evento = defineProps({
-    titulo : String,
-    precio : Number,
-    fecha: Date,
-    descripcion: String,
-    cantidad: String,
-    //Imagen: imagen
+  titulo: String,
+  precio: Number,
+  fecha: Date,
+  descripcion: String,
+  cantidad: String,
+  //Imagen: imagen
 });
 
 </script>
